a5b55bac2663ee59b85063fbcd760ad9e9feef41
Alexey Mironov <al.g.mironov@yandex.ru>
2022-02-03 22:16:00 +0300

    Initial commit

---
+++ src/main/java/pro/skystudent/streamandoptional/StreamandoptionalApplication.java
@@ -0,0 +1,13 @@
+package pro.skystudent.streamandoptional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamandoptionalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamandoptionalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/skystudent/streamandoptional/StreamandoptionalApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.skystudent.streamandoptional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamandoptionalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
